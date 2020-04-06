0297aec3a39773da525751f035888e3a54653a66
Samiylo Kryshu <skryshu@gmail.com>
2020-04-06 21:12:40 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,8 +1,11 @@
 class Owner
   
+  @@all = []
+  
   def initialize (name)
     @name = name 
     @species = "human"
+    @all << self 
   end
   
   def name 
@@ -15,5 +18,9 @@ class Owner
   
   def say_species
     "I am a #{@species}."
+  end
+  
+  def self.all 
+    @@all 
   end
 end
