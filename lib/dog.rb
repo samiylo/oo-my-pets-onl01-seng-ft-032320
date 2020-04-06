ef964ed3fe72e77af4c31b10723a3cadfe37793e
Samiylo Kryshu <skryshu@gmail.com>
2020-04-06 21:43:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,12 @@
 class Dog
   
+  attr_accessor :owner
+  
   @@all = []
   def initialize (name, owner)
     @name = name
     owner_obj = Owner.new(owner)
+    @mood = "nervous"
     @owner = owner_obj
     
     @@all << self
