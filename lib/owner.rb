00589ef6f26bbba55944371283342c50ab6ee7ff
Samiylo Kryshu <skryshu@gmail.com>
2020-04-06 21:59:55 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -49,6 +49,13 @@ class Owner
     Dog.new(name, self)
   end
   
+  def walk_dogs
+   self.dogs.each {|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    self.cats.each {|cat| cat.mood = "happy"}
+  end
   
   
   
