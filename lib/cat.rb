20f44b3e9472bf64fa2f1e6c6fc5fa987f0ed230
Samiylo Kryshu <skryshu@gmail.com>
2020-04-06 21:25:50 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,7 +2,7 @@ class Cat
   # code goes here
   attr_accessor :owner, :mood
   
-  def initialize (name, Owner)
+  def initialize (name, owner)
     @name = name
     @owner = Owner
     @mood = "nervous"
