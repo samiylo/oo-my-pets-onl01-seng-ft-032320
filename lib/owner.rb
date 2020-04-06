d590742ae8fa29a82ee6978c6ec1d966c96a8133
Samiylo Kryshu <skryshu@gmail.com>
2020-04-06 22:06:43 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -63,6 +63,12 @@ class Owner
       cat.owner = nil
       cat.mood = "nervous"
     end
+     self.dogs.each do |dog|
+      self.dogs.delete(dog)
+      dog.owner = nil
+      dog.mood = "nervous"
+    end
+  end
   
   
   
