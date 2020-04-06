f18074af463d9e8219380e3fac2ecfc16b2088da
Samiylo Kryshu <skryshu@gmail.com>
2020-04-06 21:42:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,11 @@
 class Dog
   
-  
+  @@all = []
   def initialize (name, owner)
     @name = name
-    new_owner
+    owner_obj = Owner.new(owner)
+    @owner = owner_obj
+    
+    @@all << self
   end
 end
